9580b69d958c3c16831df0f2a3978bdc8e02c072
Alyasa62 <m.alyasa62@gmail.com>
2026-06-29 11:02:03 +0500

    Initial commit for Quiz App

---
+++ quizzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+string[] text = File.ReadAllLines("questions.txt");
+
+List<string> questions = new List<string>();
+List<string> answers = new List<string>();
+
+for (int i = 0; i < text.Length; i++)
+{
+    if (i % 4 == 0)
+        questions.Add(text[i]);
+    else
+        answers.Add(text[i]);
+    
+}
+
+int questionIndex = 0, answerIndex = 0, score = 0;
+
+while (questionIndex < questions.Count)
+{
+    Console.WriteLine(questions[questionIndex]);
+    questionIndex++;
+
+    int correctAnswerIndex = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (answers[answerIndex].StartsWith(">"))
+            correctAnswerIndex = i + 1;
+        Console.WriteLine(i + 1 + "." + answers[answerIndex].Replace(">", ""));
+        answerIndex++;
+
+    }
+
+    int answer = int.Parse(Console.ReadLine());
+    if (answer == correctAnswerIndex)
+    {
+        Console.WriteLine("Correct Answer");
+        score++;
+
+    }
+    else
+        Console.WriteLine("Incorrect");
+}
+
+Console.WriteLine("Quiz ended! Your score is: " + score);
